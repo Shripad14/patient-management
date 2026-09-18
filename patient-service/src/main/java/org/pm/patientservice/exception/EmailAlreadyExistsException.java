8cb336fa5d2e8500951dc2af09bfa49940beaf12
Shripad Dhanshetti <shripad1483@gmail.com>
2026-09-18 19:38:14 +0530

    feat(EmailAlradyExistsException): custom exception logic

--- patient-service/src/main/java/org/pm/patientservice/exception/EmailAlreadyExistsException.java
+++ patient-service/src/main/java/org/pm/patientservice/exception/EmailAlreadyExistsException.java
@@ -1,7 +1,7 @@
 package org.pm.patientservice.exception;
 
 public class EmailAlreadyExistsException extends RuntimeException {
-  public EmailAlreadyExistsException(String message) {
-    super(message);
-  }
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
 }
