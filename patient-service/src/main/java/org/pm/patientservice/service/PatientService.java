da6c2bf09640192b3ba9715f3d8bcdbf29b0380a
Shripad Dhanshetti <shripad1483@gmail.com>
2026-09-23 20:35:36 +0530

    feat: add grpc client to patient service.

--- patient-service/src/main/java/org/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/org/pm/patientservice/service/PatientService.java
@@ -5,6 +5,7 @@ import org.pm.patientservice.dto.PatientResponseDTO;
 import org.pm.patientservice.entity.Patient;
 import org.pm.patientservice.exception.EmailAlreadyExistsException;
 import org.pm.patientservice.exception.PatientNotFoundException;
+import org.pm.patientservice.grpc.BillingServiceGrpcClient;
 import org.pm.patientservice.mapper.PatientMapper;
 import org.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
@@ -17,9 +18,11 @@ import java.util.UUID;
 public class PatientService {
 
     private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
 
-    public PatientService(PatientRepository patientRepository){
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient){
         this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
     }
 
     public List<PatientResponseDTO> getPatients(){
@@ -40,6 +43,9 @@ public class PatientService {
                     + " already exists.");
         }
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),
+                newPatient.getName(), newPatient.getEmail());
 
         return PatientMapper.toDTO(newPatient);
     }
